36fc1d74bc33d3bd53ddefd402258a31e9ae9fd9
CarlosV319 <carlos.alberto_88@hotmail.com>
2021-11-29 06:13:24 -0300

    final t2

--- frontend/src/components/City.js
+++ frontend/src/components/City.js
@@ -1,44 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Header from "./Header";
 
 const City = () => {
+  const [city, setCity] = useState();
+  let { id } = useParams();
 
-    const [city, setCity] = useState()
-    let { id } = useParams();
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/cities/" + id).then((res) => {
+      console.log(res);
+      setCity(res.data.respuesta);
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:4000/api/cities/'+id)
-        .then(res => {
-          console.log(res)
-          setCity(res.data.respuesta)
-        })
-    },[])
-
-
-    return (
-        <>      
-        <div className="container-city-padre">
-                {city && <div className="container-card-city">
-                    <h2>Discover the beauty of <span>{city.name}</span></h2>
-                    <div key={city._id} className="city-img-container-indiv">
-                        <img src={city.img} alt={city.name}/>
-                        <div  className="name-container">
-                            <p className="name-city">{city.name}</p>
-                            
-                        </div>
-                    </div>
-                </div>
-                }
-                <div className="city-contenido">
-                    <p>UNDER CONSTRUCTION</p>
-                    <Link to="/Cities" className="link-city"><p>BACK TO CITIES</p></Link>
-                </div>
+  return (
+    <>
+      <Header />
+      <div className="container-city">
+        {city && (
+          <div className="container-card">
+            <div key={city._id} className="city-img">
+              <img src={city.img} alt={city.name} />
+              <div className="container-n">
+                <p className="name-city">{city.name}</p>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="city-container">
+          <p>UNDER CONSTRUCTION</p>
+          <Link to="/Cities" className="link-city ">
+            <p>BACK TO CITIES</p>
+          </Link>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default City
+export default City;
